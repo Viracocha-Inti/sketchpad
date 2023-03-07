e1a85dc47d3e999d02fb2a4b2127b706304f5208
Viracocha-Inti <valenciasamir21@gmail.com>
2023-03-07 11:36:44 -0500

    Project js changed. Canvas sizefunc based on input
     Changes to be committed:
    	modified:   app.js
    	modified:   index.html
    	modified:   style.css

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-const gridContainer = document.getElementById("grid");
+function createCanvas(size) {
+  let canvas = document.querySelector(".grid");
+  let squares = canvas.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  canvas.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  canvas.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-function createCanvas16X16() {
-  for (let y = 0; y < 16; y++) {
-    const divGridY = document.createElement("div");
-    divGridY.classList.add("canvasGrid");
-    // divGridY.textContent = "Y";
-    gridContainer.appendChild(divGridY);
-    for (let x = 0; x < 15; x++) {
-      const divGridX = document.createElement("div");
-      divGridX.classList.add("canvasGrid");
-    //   divGridX.textContent = "x";
-      gridContainer.appendChild(divGridX);
-    }
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
+    });
+    square.classList.add("canvasGrid");
+    square.style.backgroundColor = "white";
+    canvas.insertAdjacentElement("beforeend", square);
+  }
+}
+function changeCanvasSize(input) {
+  if (input >= 2 && input <= 100) {
+    createCanvas(input);
+  } else if (input < 2) {
+    console.log("Not enough squares");
+  } else {
+    console.log("Too many squares");
   }
 }
 
-createCanvas16X16();
+createCanvas(16);
+
+//Function below will create the 16x16 sketchpad
+// function createCanvas16X16() {
+//   for (let y = 0; y < 16; y++) {
+//     const divGridY = document.createElement("div");
+//     divGridY.classList.add("canvasGrid");
+//     // divGridY.textContent = "Y";
+//     gridContainer.appendChild(divGridY);
+//     for (let x = 0; x < 15; x++) {
+//       const divGridX = document.createElement("div");
+//       divGridX.classList.add("canvasGrid");
+//       //   divGridX.textContent = "x";
+//       gridContainer.appendChild(divGridX);
+//     }
+//   }
+// }
+// createCanvas16X16();
